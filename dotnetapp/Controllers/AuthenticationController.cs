9304003a36b6b1762f7295ca2c6cc106e322dfa0
AnkithSheregar <ankithcsheregar@gmail.com>
2025-04-12 05:18:49 +0000

    Controllers Done

---
+++ dotnetapp/Controllers/AuthenticationController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetapp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        
+    }
+}
