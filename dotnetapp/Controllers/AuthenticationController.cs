d193e347637812b5daa39ac24c4d74d5857bf327
Deepfry53 <sdeepak8087@gmail.com>
2025-04-12 12:45:07 +0000

    authentication

--- dotnetapp/Controllers/AuthenticationController.cs
+++ dotnetapp/Controllers/AuthenticationController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using dotnetapp.Models;
+using dotnetapp.Services;
+using dotnetapp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace dotnetapp.Controllers
 {
@@ -10,6 +14,47 @@ namespace dotnetapp.Controllers
     [Route("api/[controller]")]
     public class AuthenticationController : ControllerBase
     {
-        
+
+        private readonly IAuthService _authService;
+
+        public  AuthenticationController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _authService.Login(model);
+            if (result.Item1 == 1)
+            {
+                 return Ok(new { Token = result.Item2 });
+            }
+                return StatusCode(500, result.Item2);
+
+            
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(User model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _authService.Register(model, model.UserRole);
+            if (result.Item1 == 1)
+            {
+                return Ok("User registered successfully.");
+            }
+            return StatusCode(500, result.Item2);
+        }
     }
 }
+
