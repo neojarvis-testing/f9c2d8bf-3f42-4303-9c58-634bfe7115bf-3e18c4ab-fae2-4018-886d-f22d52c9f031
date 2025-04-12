26e818eeef22d4d46c139885dfa45c2599d1310c
Deepfry53 <sdeepak8087@gmail.com>
2025-04-12 10:05:10 +0000

    iAuth and Auth services done  by deep

--- dotnetapp/Data/ApplicationDbContext.cs
+++ dotnetapp/Data/ApplicationDbContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using dotnetapp.Models;
+
 
 
 namespace dotnetapp.Data
 {
-    public class ApplicationDbContext : Dbcontext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext() : base(){}
 
@@ -19,10 +21,7 @@ namespace dotnetapp.Data
 
         public DbSet<Feedback>Feedbacks {get;set;}
 
-        public DbSet<User>Users {get;set;}
-
-        
-
+        public DbSet<User>Users{get;set;}
 
     }
 }
--- dotnetapp/Services/IAuthService.cs
+++ dotnetapp/Services/IAuthService.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnetapp.Data;
+using dotnetapp.Models;
+
 
 namespace dotnetapp.Services
 {
     public interface IAuthService
     {
-        
+        Task<(int, string)> Register(User model, string role);
+        Task<(int, string)> Login(LoginModel model);   
     }
 }
