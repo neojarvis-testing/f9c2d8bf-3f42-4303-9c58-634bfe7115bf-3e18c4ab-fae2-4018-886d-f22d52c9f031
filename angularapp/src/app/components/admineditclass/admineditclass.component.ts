171ea31a6d9a48d1f46eff842747bf5f7c3658a3
spandanasudhakar29 <spandanasudhakarsanil@gmail.com>
2025-04-12 15:39:35 +0000

    frontendcomponents

---
+++ angularapp/src/app/components/admineditclass/admineditclass.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admineditclass',
+  templateUrl: './admineditclass.component.html',
+  styleUrls: ['./admineditclass.component.css']
+})
+export class AdmineditclassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
