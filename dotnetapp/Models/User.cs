53cc2c57b6d006fa78f28a03f2af5191932c01a5
AbhimanNB <abhimanbangera@gmail.com>
2025-04-19 04:14:58 +0000

    this is a random commit

--- dotnetapp/Models/User.cs
+++ dotnetapp/Models/User.cs
@@ -10,6 +10,8 @@ namespace dotnetapp.Models
     {
         [Key]
         public int UserId{get;set;}
+        [EmailAddress]
+        [Required]
         public string Email{get;set;}
         public string Password{get;set;}
         public string Username{get;set;}
